a587746e0318a959362c2131b85913f50dd850d9
leybodv <leybodv@gmail.com>
2023-11-04 23:17:18 +0200

    new file:   pyopspec/process_config.py

---
+++ pyopspec/process_config.py
@@ -0,0 +1,40 @@
+steps = [
+            HeatingStep(
+                pressure=1,
+                flow_rates={
+                                'Ar' :15,
+                                'H2' :5,
+                                'CO' :0,
+                                'CO2':0,
+                            },
+                heating_rate=1,
+                target_temperature=445),
+            IsothermalStep(
+                pressure=1,
+                flow_rates={
+                                'Ar' :15,
+                                'H2' :5,
+                                'CO' :0,
+                                'CO2':0,
+                            },
+                time=120),
+            CoolingStep(
+                pressure=1,
+                flow_rates={
+                                'Ar' :20,
+                                'H2' :0,
+                                'CO' :0,
+                                'CO2':0,
+                            },
+                cooling_rate=1,
+                target_temperature=276),
+            FinalStep(
+                pressure=3,
+                flow_rates={
+                                'Ar' :5,
+                                'H2' :5,
+                                'CO' :5,
+                                'CO2':5,
+                            },
+                temperature=276),
+        ]
