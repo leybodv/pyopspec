558abcba74ed1a831c7f0baa64b8551279235326
leybodv <leybodv@gmail.com>
2023-11-06 22:00:11 +0200

    new file:   pyopspec/steps/isothermal_step.py

---
+++ pyopspec/steps/isothermal_step.py
@@ -0,0 +1,11 @@
+class IsothermalStep():
+    """
+    """
+
+    def __init__(self, pressure:int, flow_rates:dict[str, float], time:int):
+        """
+        """
+
+        self.pressure = pressure
+        self.flow_rates = flow_rates
+        self.time = time
