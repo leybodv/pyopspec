d78b550fe07e9e9c06659e50c75b80712a18d402
leybodv <leybodv@gmail.com>
2023-11-07 17:44:49 +0200

    set_pressure: added method implementation

--- pyopspec/pressure_controller/bronkhorst_pressure_controller.py
+++ pyopspec/pressure_controller/bronkhorst_pressure_controller.py
@@ -13,11 +13,22 @@ class BronkhorstPressureController():
         """
         """
         self._propar_instrument = propar.instrument(comport=self._port, address=self._address)
-        if self._propar_instrument.readParameter(92) != self._serial_number:
-            raise WrongDeviceException()
+        device_sn = self._propar_instrument.readParameter(92)
+        if device_sn != self._serial_number:
+            raise WrongDeviceException(f'Trying to connect device with S/N {self._serial_number} to the device with S/N {device_sn}')
         self._connected = True
-        max_controlled_pressure = self._propar_instrument.readParameter(21)
-        pressure_unit = self._propar_instrument.readParameter(129)
+        self._propar_instrument.wink()
+        self._max_controlled_pressure = self._propar_instrument.readParameter(21)
+        self._pressure_unit = self._propar_instrument.readParameter(129)
         setpoint = self._propar_instrument.readParameter(206)
         measure = self._propar_instrument.readParameter(205)
-        self._logger.info(f'Connected to pressure controller. Max controlled pressure: {max_controlled_pressure} {pressure_unit}. Current setpoint value: {setpoint} {pressure_unit}. Current measured value: {measure} {pressure_unit}')
+        self._logger.info(f'Connected to pressure controller. Max controlled pressure: {self._max_controlled_pressure} {self._pressure_unit}. Current setpoint value: {setpoint} {self._pressure_unit}. Current measured value: {measure} {self._pressure_unit}')
+
+    def set_pressure(self, pressure:float):
+        """
+        """
+        if not self._connected:
+            raise WrongDeviceStateException(f'Device with S/N {self._serial_number} is not connected')
+        if pressure > self._max_controlled_pressure:
+            raise OutOfDeviceCapacityException(f'Trying to set pressure {pressure} {self._pressure_unit} on a device with max capacity of {self._max_controlled_pressure} {self._pressure_unit}')
+        self._propar_instrument.writeParameter(dde_nr=206, data=pressure)
