73fbaec5bf6c565a0bef7e07ccaf2a0633cb59a0
leybodv <leybodv@gmail.com>
2023-11-08 21:30:03 +0200

    removed mfc classes from pycatalicism

--- pyopspec/mass_flow_controller/logging_config.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-# Dictionary of logging levels of corresponding classes
-logging_levels = {
-                'BronkhorstF201CV'	:	logging.INFO,
-                }
--- pyopspec/mass_flow_controller/mfc_exceptions.py
+++
@@ -1,9 +0,0 @@
-class MFCConnectionException(Exception):
-    """
-    Class represents exceptions related to connection errors
-    """
-
-class MFCStateException(Exception):
-    """
-    Class represents exceptions related to wrong state of the device
-    """
