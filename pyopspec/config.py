70c31b7a151389b19f9677abba8da8febc27fc6e
leybodv <leybodv@gmail.com>
2023-11-08 21:44:20 +0200

    added import

--- pyopspec/config.py
+++ pyopspec/config.py
@@ -1,4 +1,5 @@
 from pyopspec.pressure_controller.bronkhorst_pressure_controller import BronkhorstPressureController
+from pyopspec.mass_flow_controller.bronkhorst_mass_flow_controller import BronkhorstMassFlowController
 
 pressure_controller = BronkhorstPressureController(
                                                     port='',
