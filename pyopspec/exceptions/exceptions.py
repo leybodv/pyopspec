1e9411ec932f7e95f0ae83ea2416174d9b180ca3
leybodv <leybodv@gmail.com>
2023-11-13 17:34:36 +0200

    kept only projectwise exception

--- pyopspec/exceptions/exceptions.py
+++ pyopspec/exceptions/exceptions.py
@@ -2,18 +2,3 @@ class UnknownStepException(Exception):
     """
     """
 
-class WrongDeviceException(Exception):
-    """
-    """
-
-class NotSupportedException(Exception):
-    """
-    """
-
-class WatlowProtocolException(Exception):
-    """
-    """
-
-class WrongDeviceStateException(Exception):
-    """
-    """
