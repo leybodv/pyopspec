13a22a90744824963d88340993af00300589611f
leybodv <leybodv@gmail.com>
2023-11-13 17:02:40 +0200

    added abstract methods

--- pyopspec/furnace/furnace_controller.py
+++ pyopspec/furnace/furnace_controller.py
@@ -5,5 +5,25 @@ class FurnaceController(ABC):
     """
 
     @abstractmethod
+    def connect(self):
+        raise NotImplementedError()
+
+    @abstractmethod
     def get_temperature(self) -> float:
-        pass
+        raise NotImplementedError()
+
+    @abstractmethod
+    def set_ramp_rate(self, ramp_rate:float):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def set_temperature(self, temperature:float):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def heat_up_to(self, target_temperature:float):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def cool_down_to(self, target_temperature:float):
+        raise NotImplementedError()
