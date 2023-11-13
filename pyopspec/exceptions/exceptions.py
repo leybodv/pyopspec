f448e26073e11e05473b6d827e01745addb0fac0
leybodv <leybodv@gmail.com>
2023-11-13 17:29:34 +0200

    added exceptions from Watlow

--- pyopspec/exceptions/exceptions.py
+++ pyopspec/exceptions/exceptions.py
@@ -1,3 +1,19 @@
 class UnknownStepException(Exception):
     """
     """
+
+class WrongDeviceException(Exception):
+    """
+    """
+
+class NotSupportedException(Exception):
+    """
+    """
+
+class WatlowProtocolException(Exception):
+    """
+    """
+
+class WrongDeviceStateException(Exception):
+    """
+    """
