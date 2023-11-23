7d3d7589cb199aa593104c12392203582ab19ce0
leybodv <leybodv@gmail.com>
2023-11-23 17:51:30 +0200

    added install_requires dependencies

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 install_requires = [
             "bronkhorst-propar>=1.0,<2.0",
+            "pywatlow>=0.1.4",
+            "matplotlib>=3.8,<4.0",
     ]
 
 setuptools.setup(
