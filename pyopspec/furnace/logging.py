855f135c0c3eb1ffb560b79c9ac618f66539618f
leybodv <leybodv@gmail.com>
2024-03-19 15:28:49 +0200

    log to file

--- pyopspec/furnace/logging.py
+++ pyopspec/furnace/logging.py
@@ -3,12 +3,13 @@ Module for instantiating and configuring logger
 """
 
 import logging
+from datetime import datetime
 
 logging_levels = {
                     'WatlowFurnaceController':logging.INFO
                  }
 
-def get_logger(name:str) -> logging.Logger:
+def get_logger(name:str, logdir:str) -> logging.Logger:
     """
     Get logger with corresponding name configured to log to stdout.
 
@@ -26,7 +27,8 @@ def get_logger(name:str) -> logging.Logger:
     logger.setLevel(logging_levels[name])
     logger.propagate = False
 
-    ch = logging.StreamHandler()
+    # ch = logging.StreamHandler()
+    ch = logging.FileHandler(filename=logdir + f'/{datetime.now():%Y%m%d_%H%M%S}.log')
     ch.setLevel(logging_levels[name])
 
     formatter = logging.Formatter(fmt='[%(asctime)s] %(name)s.%(funcName)s: %(levelname)s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
