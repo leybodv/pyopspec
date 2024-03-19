62c78a09b5bc2c7719b5f93c480754a0504d5ba7
leybodv <leybodv@gmail.com>
2024-03-19 16:49:55 +0200

    log to a file

--- pyopspec/config.py
+++ pyopspec/config.py
@@ -4,6 +4,7 @@ from pyopspec.pressure_controller.bronkhorst_pressure_controller import Bronkhor
 from pyopspec.mass_flow_controller.bronkhorst_mass_flow_controller import BronkhorstMassFlowController
 from pyopspec.furnace.watlow_furnace_controller import WatlowFurnaceController
 
+logfilename=f'./pyopspec_logs/{datetime.now():%Y%m%d_%H%M%S}.log'
 pressure_controller = BronkhorstPressureController(
                                                     port='COM6',
                                                     serial_number='M22202037R',
@@ -14,28 +15,33 @@ mfcs = {
         'Ar':BronkhorstMassFlowController(
                                            port='COM6',
                                            serial_number='M21222867K',
+                                           logfilename=logfilename,
                                            address=15,
                                          ),
         'CO2':BronkhorstMassFlowController(
                                            port='COM6',
                                            serial_number='M21222867G',
+                                           logfilename=logfilename,
                                            address=10,
                                          ),
         'CO':BronkhorstMassFlowController(
                                            port='COM6',
                                            serial_number='M21222867B',
+                                           logfilename=logfilename,
                                            address=100,
                                          ),
         'H2':BronkhorstMassFlowController(
                                            port='COM6',
                                            serial_number='M21222867I',
+                                           logfilename=logfilename,
                                            address=13,
                                          ),
         'N2':BronkhorstMassFlowController(
                                            port='COM6',
                                            serial_number='M21222867J',
+                                           logfilename=logfilename,
                                            address=14,
                                          ),
       }
 
-furnace = WatlowFurnaceController(serial_number='12345', port='COM3', logfilename=f'./pyopspec_logs/{datetime.now():%Y%m%d_%H%M%S}.log')
+furnace = WatlowFurnaceController(serial_number='12345', port='COM3', logfilename=logfilename)
