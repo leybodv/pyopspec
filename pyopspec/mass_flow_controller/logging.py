e7c14f02c295a55fe86626ce2e6b1764ea9f7722
leybodv <leybodv@gmail.com>
2024-03-19 16:46:48 +0200

    log to a file

--- pyopspec/mass_flow_controller/logging.py
+++ pyopspec/mass_flow_controller/logging.py
@@ -3,12 +3,13 @@ Module for instantiating and configuring logger
 """
 
 import logging
+from pathlib import Path
 
 logging_levels = {
                     'BronkhorstMassFlowController':logging.INFO,
                  }
 
-def get_logger(name:str) -> logging.Logger:
+def get_logger(name:str, logfilename:str) -> logging.Logger:
     """
     Get logger with corresponding name configured to log to stdout.
 
@@ -26,7 +27,10 @@ def get_logger(name:str) -> logging.Logger:
     logger.setLevel(logging_levels[name])
     logger.propagate = False
 
-    ch = logging.StreamHandler()
+    # ch = logging.StreamHandler()
+    logfile = Path(logfilename).absolute()
+    logfile.parent.mkdir(parents=True, exist_ok=True)
+    ch = logging.FileHandler(filename=logfile)
     ch.setLevel(logging_levels[name])
 
     formatter = logging.Formatter(fmt='[%(asctime)s] %(name)s.%(funcName)s: %(levelname)s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
